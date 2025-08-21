ef6a29013abedda5d6da0b3c9afa9d914861361c
Steven Ulloa <89059916+StevenU21@users.noreply.github.com>
2025-08-21 09:13:56 -0600

    Add Profile model and migration, and establish relationship with User model

---
+++ app/Models/Profile.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Profile extends Model
+{
+    protected $fillable = [
+        'avatar',
+        'phone',
+        'identity_card',
+        'gender',
+        'address',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
