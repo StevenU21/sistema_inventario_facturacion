becde7d6ad08f3f7423e8e97b06f34f9785c428d
Steven Ulloa <89059916+StevenU21@users.noreply.github.com>
2025-08-25 23:12:10 -0600

    feat: add controllers and models for Brand, Category, Client, Company, PaymentMethod, ProductStatus, Supplier, Tax, and UnitMeasure

---
+++ database/migrations/2025_08_25_230625_create_companies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('ruc')->unique();
+            $table->string('logo')->nullable();
+            $table->string('description')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
