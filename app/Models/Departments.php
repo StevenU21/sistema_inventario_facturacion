4e5138a0790772ed2630c1fb5c636c771e7aebf7
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-08-27 07:55:46 -0600

    feat: add LogsActivity trait and activity log options to Departments and Municipality models

--- app/Models/Departments.php
+++ app/Models/Departments.php
@@ -4,15 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Departments extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $fillable = [
         'name',
         'description',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'description']);
+    }
 
     public function getFormattedCreatedAtAttribute(): ?string
     {
--- app/Models/Municipality.php
+++ app/Models/Municipality.php
@@ -4,16 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Municipality extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $fillable = [
         'name',
         'description',
         'department_id',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'description', 'department_id']);
+    }
 
     public function getFormattedCreatedAtAttribute(): ?string
     {
