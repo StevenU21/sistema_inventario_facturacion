ea7e89d7b4b6bd38c121899f6290f5ec0129194b
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-08-27 13:13:20 -0600

    feat: add activity logging and formatted date attributes to Entity model

--- app/Models/Entity.php
+++ app/Models/Entity.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Entity extends Model
 {
+    use HasFactory, LogsActivity;
     protected $fillable = [
         'first_name',
         'last_name',
@@ -19,4 +23,20 @@ class Entity extends Model
         'is_supplier',
         'is_active'
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['first_name', 'last_name', 'identity_card', 'ruc', 'email', 'phone', 'address', 'description', 'is_client', 'is_supplier', 'is_active']);
+    }
+
+    public function getFormattedCreatedAtAttribute(): ?string
+    {
+        return $this->created_at ? $this->created_at->format('d/m/Y H:i:s') : null;
+    }
+
+    public function getFormattedUpdatedAtAttribute(): ?string
+    {
+        return $this->updated_at ? $this->updated_at->format('d/m/Y H:i:s') : null;
+    }
 }
