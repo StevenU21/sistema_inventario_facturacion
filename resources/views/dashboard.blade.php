540b97b47d24b305423f18ba1f74055b5bd37d83
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-09-04 09:33:00 -0600

    feat: Update product label in dashboard to use Spanish for improved localization

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,7 +37,7 @@
                 </div>
                 <div>
                     <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                        Total products
+                        Total productos
                     </p>
                     <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
                         {{ $products }}
