c3892eb806b6839b37eedb08b30ade981aa9dce4
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-09-04 11:47:49 -0600

    feat: Add search routes for categories, brands, unit measures, payment methods, taxes, roles, and enhance inventory management

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\WarehouseController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('auth/login');
@@ -28,6 +27,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
+    // Profile Routes
     Route::prefix('/profile')->name('profile.')->group(function () {
         Route::get('/', [ProfileController::class, 'show'])->name('show');
         Route::get('/edit', [ProfileController::class, 'edit'])->name('edit');
@@ -55,37 +55,53 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('admin/backups', [BackupController::class, 'index'])->name('backups.index');
     Route::post('admin/backups/restore', [BackupController::class, 'restore'])->name('backups.restore');
 
+    // Categories
     Route::get('categories/search', [CategoryController::class, 'search'])->name('categories.search');
     Route::resource('categories', CategoryController::class);
 
+    // Brands
     Route::get('brands/search', [BrandController::class, 'search'])->name('brands.search');
     Route::resource('brands', BrandController::class);
 
+    // Companies
     Route::resource('companies', CompanyController::class)->except(['destroy']);
 
+    // Unit Measures
     Route::get('unit_measures/search', [UnitMeasureController::class, 'search'])->name('unit_measures.search');
     Route::resource('unit_measures', UnitMeasureController::class);
 
+    // Payment Methods
     Route::get('payment_methods/search', [PaymentMethodController::class, 'search'])->name('payment_methods.search');
     Route::resource('payment_methods', PaymentMethodController::class);
 
+    // Taxes
     Route::get('taxes/search', [TaxController::class, 'search'])->name('taxes.search');
-    
+
+    // Taxes
     Route::resource('taxes', TaxController::class);
 
+    // Entities
     Route::resource('entities', EntityController::class);
 
+    // Products
     Route::get('products/search', [ProductController::class, 'search'])->name('products.search');
     Route::get('products/export', [ProductController::class, 'export'])->name('products.export');
     Route::resource('products', ProductController::class);
 
+    // Roles
     Route::get('roles/search', [RoleController::class, 'search'])->name('roles.search');
     Route::resource('roles', RoleController::class);
-    
+
+    // Warehouses
     Route::resource('warehouses', WarehouseController::class);
+
+    // Inventories
     Route::resource('inventories', InventoryController::class);
+
+    // Inventory Movements
     Route::resource('inventory_movements', InventoryMovementController::class);
 
+    // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 });
 
