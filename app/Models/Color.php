341ed5073a10dbda3e38155a156108617becfea8
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-09-08 08:06:07 -0600

    feat: Add Color and Size models with activity logging and formatted date attributes

---
+++ app/Models/Color.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Color extends Model
+{
+    use HasFactory, LogsActivity;
+
+    protected $fillable = [
+        'name',
+        'hex_code'
+    ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'hex_code']);
+    }
+
+    public function getFormattedCreatedAtAttribute(): ?string
+    {
+        return $this->created_at ? $this->created_at->format('d/m/Y H:i:s') : null;
+    }
+
+    public function getFormattedUpdatedAtAttribute(): ?string
+    {
+        return $this->updated_at ? $this->updated_at->format('d/m/Y H:i:s') : null;
+    }
+}
