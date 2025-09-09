f289fa2dc1d89c3ec40fb3b910e423ca80260322
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-09-09 10:45:46 -0600

    feat: Implement Purchase and PurchaseDetail models with corresponding migration files for database structure

---
+++ database/migrations/2025_09_09_103622_create_purchases_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('status', ['pending', 'completed', 'canceled'])->default('pending');
+            $table->date('purchase_date')->nullable();
+            $table->date('expected_at')->nullable();
+            $table->date('received_at')->nullable();
+            $table->string('reference')->nullable();
+            $table->string('document_number')->nullable();
+            $table->decimal('subtotal', 10, 2)->nullable();
+            $table->decimal('total', 10, 2)->nullable();
+
+            $table->integer('warehouse_id')->unsigned();
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->integer('payment_method_id')->unsigned();
+            $table->foreign('payment_method_id')->references('id')->on('payment_methods')->onDelete('cascade')->onUpdate('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
---
+++ database/migrations/2025_09_09_104419_create_purchase_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_details', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('purchase_id')->unsigned();
+            $table->foreign('purchase_id')->references('id')->on('purchases')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_details');
+    }
+};
