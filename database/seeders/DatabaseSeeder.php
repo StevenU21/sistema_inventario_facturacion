06126a087205c1b87c97634190e317ce4452037e
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-09-09 15:27:43 -0600

    feat: Uncomment seeder calls for various models in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -52,59 +52,59 @@ class DatabaseSeeder extends Seeder
         ]);
         $cashierUser->assignRole('cashier');
 
-        Category::factory()->count(12)->create();
-        Brand::factory()->count(13)->create();
-        Company::factory()->count(1)->create();
-        UnitMeasure::factory()->count(4)->create();
-        Department::factory()->count(10)->create();
-        Municipality::factory()->count(35)->create();
-        PaymentMethod::factory()->count(3)->create();
-        Tax::factory()->count(3)->create();
-        Entity::factory()->count(20)->create();
-        Size::factory()->count(8)->create();
-        Color::factory()->count(6)->create();
+        // Category::factory()->count(12)->create();
+        // Brand::factory()->count(13)->create();
+        // Company::factory()->count(1)->create();
+        // UnitMeasure::factory()->count(4)->create();
+        // Department::factory()->count(10)->create();
+        // Municipality::factory()->count(35)->create();
+        // PaymentMethod::factory()->count(3)->create();
+        // Tax::factory()->count(3)->create();
+        // Entity::factory()->count(20)->create();
+        // Size::factory()->count(8)->create();
+        // Color::factory()->count(6)->create();
 
-        Product::factory()->count(100)->create()->each(function ($product) {
-            $variantsCount = rand(1, 5);
-            for ($i = 0; $i < $variantsCount; $i++) {
-                ProductVariant::factory()->create([
-                    'product_id' => $product->id
-                ]);
-            }
-        });
+        // Product::factory()->count(100)->create()->each(function ($product) {
+        //     $variantsCount = rand(1, 5);
+        //     for ($i = 0; $i < $variantsCount; $i++) {
+        //         ProductVariant::factory()->create([
+        //             'product_id' => $product->id
+        //         ]);
+        //     }
+        // });
 
-        Purchase::factory()->count(20)->create()->each(function ($purchase) {
-            $detailsCount = rand(1, 5);
-            $subtotal = 0;
-            for ($i = 0; $i < $detailsCount; $i++) {
-                $variant = ProductVariant::inRandomOrder()->first();
-                $quantity = rand(1, 10);
-                $unitPrice = $variant->price ?? rand(10, 100);
-                $lineTotal = $quantity * $unitPrice;
-                $subtotal += $lineTotal;
-                PurchaseDetail::factory()->create([
-                    'purchase_id' => $purchase->id,
-                    'product_variant_id' => $variant->id,
-                    'quantity' => $quantity,
-                    'unit_price' => $unitPrice,
-                ]);
-            }
-            $purchase->subtotal = $subtotal;
-            $purchase->total = $subtotal;
-            $purchase->save();
-        });
+        // Purchase::factory()->count(20)->create()->each(function ($purchase) {
+        //     $detailsCount = rand(1, 5);
+        //     $subtotal = 0;
+        //     for ($i = 0; $i < $detailsCount; $i++) {
+        //         $variant = ProductVariant::inRandomOrder()->first();
+        //         $quantity = rand(1, 10);
+        //         $unitPrice = $variant->price ?? rand(10, 100);
+        //         $lineTotal = $quantity * $unitPrice;
+        //         $subtotal += $lineTotal;
+        //         PurchaseDetail::factory()->create([
+        //             'purchase_id' => $purchase->id,
+        //             'product_variant_id' => $variant->id,
+        //             'quantity' => $quantity,
+        //             'unit_price' => $unitPrice,
+        //         ]);
+        //     }
+        //     $purchase->subtotal = $subtotal;
+        //     $purchase->total = $subtotal;
+        //     $purchase->save();
+        // });
 
-        // $this->call(CategorySeeder::class);
-        // $this->call(BrandSeeder::class);
-        // $this->call(CompanySeeder::class);
-        // $this->call(UnitMeasureSeeder::class);
-        // $this->call(DepartmentSeeder::class);
-        // $this->call(PaymentMethodSeeder::class);
-        // $this->call(TaxSeeder::class);
-        // $this->call(EntitySeeder::class);
-        // $this->call(WarehouseSeeder::class);
-        // $this->call(ProductSeeder::class);
-        // $this->call(SizeSeeder::class);
-        // $this->call(ColorSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(BrandSeeder::class);
+        $this->call(CompanySeeder::class);
+        $this->call(UnitMeasureSeeder::class);
+        $this->call(DepartmentSeeder::class);
+        $this->call(PaymentMethodSeeder::class);
+        $this->call(TaxSeeder::class);
+        $this->call(EntitySeeder::class);
+        $this->call(WarehouseSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(SizeSeeder::class);
+        $this->call(ColorSeeder::class);
     }
 }
